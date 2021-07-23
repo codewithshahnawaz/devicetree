7294d5f4a60979149973b931368ed5e3d07b3bff
codewithshahnawaz <shahnawaz28april@gmail.com>
2021-07-23 11:01:12 +0530

    made changes, added form!

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,13 +2,39 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        height: "8rem",
+        padding:"1rem"
+      }}
+    >
       {/* <h1 style={{ margin: "1rem", fontSize: "1.5rem", fontWeight:"lighter" }}>
         DeviceTree
       </h1>{" "} */}
       <h1 style={{ margin: "1rem", fontSize: "1.8rem", color: "#fa314a" }}>
         DeviceTree
       </h1>
+      <div
+        style={{
+          margin: "1rem",
+          fontSize: "1.5rem",
+          marginLeft: "1rem",
+          height: "5rem",
+          width: "5rem",
+          textAlign: "center",
+          lineHeight: "3rem",
+          backgroundColor: "pink",
+          borderRadius: "100%",
+          color: "#fa314a",
+          fontWeight: "bold",
+          cursor: "pointer"
+        }}
+      >
+        <p>SH</p>
+      </div>
     </div>
   );
 };
--- src/components/customerForm/form.js
+++ src/components/customerForm/form.js
@@ -22,43 +22,76 @@ export const TextArea = () => {
   const onStop = (recordedBlob) => {
     setVoiceData(recordedBlob.blobURL);
   };
+
   return (
     <div className="form">
       <textarea
         name="customerDetails"
         placeholder="Write your issue"
         maxLength="500"
-      /> 
+      />
       <b className="subtitle">or</b> <br />
       <b className="subtitle">Record your voice</b>
       <div className="mergeWithMicro">
-      {voiceData === "" ? (
-        <ReactMic
-          record={recording}
-          className="sound-wave"
-          onStop={onStop}
-          echoCancellation={true}
-          autoGainControl={true}
-          noiseSuppression={true}
-          visualSetting="frequencyBars"
-          mimeType="audio/wav"
-          strokeColor="#000000"
-          backgroundColor="#f7f7f7"
-        />
-      ) : (
-        <a
-          href={voiceData}
-          download="DeviceTreeIssueRecord"
-          className="voice_link"
+        {voiceData === "" ? (
+          <div
+            style={{
+              display: recording ? "flex" : "none",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <ReactMic
+              record={recording}
+              className="sound-wave"
+              onStop={onStop}
+              echoCancellation={true}
+              autoGainControl={true}
+              noiseSuppression={true}
+              visualSetting="frequencyBars"
+              mimeType="audio/wav"
+              strokeColor="#000000"
+              backgroundColor="#f7f7f7"
+              style={{ display: "none" }}
+            />
+          </div>
+        ) : (
+          <div className="downlaod">
+            <audio controls autoplay className="voice_link">
+              <source src={voiceData} type="audio/wav" />
+            </audio>
+          </div>
+        )}
+        {recording ? (
+          <BsFillStopFill
+            className="icon-mic"
+            onClick={toggleRecording}
+            style={{ color: "#fa314a" }}
+          />
+        ) : (
+          <BsFillMicFill
+            className="icon-mic"
+            onClick={toggleRecording}
+            style={{
+              color: "#fa314a",
+              marginBottom: "5rem",
+              marginLeft: "32.5rem",
+            }}
+          />
+        )}
+        <button
+          style={{
+            padding: "1rem",
+            width: "15%",
+            marginTop: "2rem",
+            marginRight: "6rem",
+            position:"absolute",
+            top:'5rem',
+            left:"30rem",
+          }}
         >
-          Your Voice Data
-        </a>
-      )}
-      {recording ? (
-        <BsFillStopFill className="icon-mic" onClick={toggleRecording} />
-      ) : (
-        <BsFillMicFill className="icon-mic" onClick={toggleRecording} />
-      )}
+          Next
+        </button>
       </div>
     </div>
   );
